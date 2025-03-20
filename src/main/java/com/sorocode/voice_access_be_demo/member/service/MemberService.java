9cd4346bf23bb4eca8cd2bafbe4209bdc65be9d7
sorocode <tudgus010707@naver.com>
2025-03-21 01:23:26 +0900

    :goal_net: IO exception 추가

--- src/main/java/com/sorocode/voice_access_be_demo/member/service/MemberService.java
+++ src/main/java/com/sorocode/voice_access_be_demo/member/service/MemberService.java
@@ -3,12 +3,14 @@ package com.sorocode.voice_access_be_demo.member.service;
 import com.sorocode.voice_access_be_demo.member.dto.SignUpRequestDto;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 public interface MemberService {
     void validateVoiceFile(MultipartFile voiceFile);
 
-    String saveVoiceFile(MultipartFile voiceFile);
+    String saveVoiceFile(MultipartFile voiceFile) throws IOException;
 
-    void saveNewMember(SignUpRequestDto signUpRequestDto, MultipartFile voiceFile);
+    void saveNewMember(SignUpRequestDto signUpRequestDto, MultipartFile voiceFile) throws IOException;
 
     String processAudioFile(MultipartFile audioFile);
 
