32105ddf52b3fc79eef48d16c114021ff45081d9
sorocode <tudgus010707@naver.com>
2025-03-21 08:06:23 +0900

    :sparkles: 생성일자, 수정일자 추가해주는 Base Entity 추가 및 기타 필드 추가

--- src/main/java/com/sorocode/voice_access_be_demo/VoiceAccessBeDemoApplication.java
+++ src/main/java/com/sorocode/voice_access_be_demo/VoiceAccessBeDemoApplication.java
@@ -2,12 +2,14 @@ package com.sorocode.voice_access_be_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class VoiceAccessBeDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VoiceAccessBeDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VoiceAccessBeDemoApplication.class, args);
+    }
 
 }
--- src/main/java/com/sorocode/voice_access_be_demo/member/entity/Member.java
+++ src/main/java/com/sorocode/voice_access_be_demo/member/entity/Member.java
@@ -1,13 +1,21 @@
 package com.sorocode.voice_access_be_demo.member.entity;
 
+import com.sorocode.voice_access_be_demo.global.BaseEntity;
+import com.sorocode.voice_access_be_demo.member.enums.GenderEnum;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
 
 @Entity
 @RequiredArgsConstructor
-@Data
-public class Member {
+@Setter
+@Getter
+public class Member extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -17,9 +25,19 @@ public class Member {
 
     @Column(nullable = false)
     private String phoneNumber;
-    
+
     private String address;
 
+    @Enumerated(EnumType.STRING)
+    private GenderEnum gender;
+
+    private LocalDate birthday;
+
+    private Short height;
+
+    private Short weight;
+
     private String voiceFileLocation;
+
 
 }
