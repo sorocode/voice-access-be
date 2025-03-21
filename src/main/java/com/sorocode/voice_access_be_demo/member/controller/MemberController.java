f93708cae46d84db84b9989251c4c0223c2ba2f5
sorocode <tudgus010707@naver.com>
2025-03-21 14:25:54 +0900

    :art: Controller -> RestController

--- src/main/java/com/sorocode/voice_access_be_demo/member/controller/MemberController.java
+++ src/main/java/com/sorocode/voice_access_be_demo/member/controller/MemberController.java
@@ -6,7 +6,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
