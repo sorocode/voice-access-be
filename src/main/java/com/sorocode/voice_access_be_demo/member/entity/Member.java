0a0a88659e3859e83e31cb069534327184fb5dbd
sorocode <tudgus010707@naver.com>
2025-03-21 20:37:55 +0900

    :art: 엔티티에 @RequiredArgsConstructor 제거

--- src/main/java/com/sorocode/voice_access_be_demo/member/entity/Member.java
+++ src/main/java/com/sorocode/voice_access_be_demo/member/entity/Member.java
@@ -4,7 +4,6 @@ import com.sorocode.voice_access_be_demo.enter_log.entity.EnterLog;
 import com.sorocode.voice_access_be_demo.global.BaseEntity;
 import com.sorocode.voice_access_be_demo.member.enums.GenderEnum;
 import jakarta.persistence.*;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
@@ -12,7 +11,6 @@ import java.util.List;
 
 
 @Entity
-@RequiredArgsConstructor
 @Setter
 public class Member extends BaseEntity {
     @Id
