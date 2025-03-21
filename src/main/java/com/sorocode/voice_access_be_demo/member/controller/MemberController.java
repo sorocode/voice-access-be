b1f9fa8a8de5362d2be208f19e6b22e06ab0ebac
sorocode <tudgus010707@naver.com>
2025-03-21 14:25:40 +0900

    :art: Controller -> RestController

--- src/main/java/com/sorocode/voice_access_be_demo/member/controller/MemberController.java
+++ src/main/java/com/sorocode/voice_access_be_demo/member/controller/MemberController.java
@@ -7,15 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class MemberController {
