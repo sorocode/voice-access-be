a973a0fd720dd843b8967118e350907634ad57f1
sorocode <tudgus010707@naver.com>
2025-03-27 14:39:36 +0900

    :art: optimize imports

--- src/main/java/com/sorocode/voice_access_be_demo/member/dto/PatchRequestDto.java
+++ src/main/java/com/sorocode/voice_access_be_demo/member/dto/PatchRequestDto.java
@@ -2,7 +2,6 @@ package com.sorocode.voice_access_be_demo.member.dto;
 
 import com.sorocode.voice_access_be_demo.member.enums.GenderEnum;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
