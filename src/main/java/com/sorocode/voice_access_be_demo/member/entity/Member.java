ec67d03cb10df5906476929a0638c61ef478f626
sorocode <tudgus010707@naver.com>
2025-03-27 16:10:07 +0900

    :sparkles: 빌더패턴 도입

--- src/main/java/com/sorocode/voice_access_be_demo/member/entity/Member.java
+++ src/main/java/com/sorocode/voice_access_be_demo/member/entity/Member.java
@@ -4,17 +4,18 @@ import com.sorocode.voice_access_be_demo.enter_log.entity.EnterLog;
 import com.sorocode.voice_access_be_demo.global.BaseEntity;
 import com.sorocode.voice_access_be_demo.member.enums.GenderEnum;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
-// TODO: 추후 빌더 패턴으로 리팩토링하기(Setter 최소화)
 @Entity
-@Setter
-@Getter
+@Getter // Setter 제거
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자 제한
+@AllArgsConstructor // 모든 필드를 받는 생성자 추가
+@Builder // 빌더 패턴 적용
 public class Member extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -36,8 +37,21 @@ public class Member extends BaseEntity {
 
     private Float weight;
 
-
     @OneToMany(mappedBy = "member")
     private List<EnterLog> enterLogs;
 
+    // 값 변경을 위한 update 메서드 추가 (Setter 대신 사용)
+    public Member update(String name, String phoneNumber, String address, GenderEnum gender, LocalDate birthday, Float height, Float weight) {
+        return Member.builder()
+                .id(this.id) // 기존 ID 유지
+                .name(name)
+                .phoneNumber(phoneNumber)
+                .address(address)
+                .gender(gender)
+                .birthday(birthday)
+                .height(height)
+                .weight(weight)
+                .enterLogs(this.enterLogs) // 기존 로그 데이터 유지
+                .build();
+    }
 }
