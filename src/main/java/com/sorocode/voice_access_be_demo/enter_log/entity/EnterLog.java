6778ac74bb71d537625277e6b09c70664e246ee5
sorocode <tudgus010707@naver.com>
2025-03-27 21:30:44 +0900

    :art: optimize imports

--- src/main/java/com/sorocode/voice_access_be_demo/enter_log/entity/EnterLog.java
+++ src/main/java/com/sorocode/voice_access_be_demo/enter_log/entity/EnterLog.java
@@ -1,10 +1,8 @@
 package com.sorocode.voice_access_be_demo.enter_log.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.sorocode.voice_access_be_demo.global.BaseEntity;
 import com.sorocode.voice_access_be_demo.member.entity.Member;
 import jakarta.persistence.*;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -27,6 +25,5 @@ public class EnterLog extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false) // Member와 연결
     private Member member;
-
 
 }
