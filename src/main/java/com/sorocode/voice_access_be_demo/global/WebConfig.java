30bb993ba8b27f9c19b1bb5685b5dedd1ba04154
sorocode <tudgus010707@naver.com>
2025-05-17 20:55:32 +0900

    :wrench: cors 관련 허용 url 설정

--- src/main/java/com/sorocode/voice_access_be_demo/global/WebConfig.java
+++ src/main/java/com/sorocode/voice_access_be_demo/global/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://10.0.2.2:8080", "http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
