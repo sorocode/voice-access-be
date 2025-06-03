22ef9af8adac5034fc0682c8ba3c2351dbe955ab
sorocode <tudgus010707@naver.com>
2025-06-03 10:24:17 +0900

    :bug: timezone 한국으로 설정

--- src/main/java/com/sorocode/voice_access_be_demo/VoiceAccessBeDemoApplication.java
+++ src/main/java/com/sorocode/voice_access_be_demo/VoiceAccessBeDemoApplication.java
@@ -1,15 +1,22 @@
 package com.sorocode.voice_access_be_demo;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class VoiceAccessBeDemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VoiceAccessBeDemoApplication.class, args);
+    }
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
