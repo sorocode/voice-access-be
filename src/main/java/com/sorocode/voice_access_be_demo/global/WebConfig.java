ac64916a211f8da99cd2e7868e183df79fe30344
sorocode <tudgus010707@naver.com>
2025-06-04 18:33:09 +0900

    :bug: 허용 origin 추가

--- src/main/java/com/sorocode/voice_access_be_demo/global/WebConfig.java
+++ src/main/java/com/sorocode/voice_access_be_demo/global/WebConfig.java
@@ -1,16 +1,19 @@
 package com.sorocode.voice_access_be_demo.global;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://10.0.2.2:8080", "http://localhost:8080", "http://localhost:3000")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
